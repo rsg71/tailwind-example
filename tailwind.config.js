4804e3dad2a7b34ea09a45a1c363cb7a0af90705
Robert Greenawalt <robert.greenawalt29@gmail.com>
2024-04-14 21:54:18 -0400

    Add more to page

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['dist/index.html'],
+  theme: {
+    extend: {},
+    container: {
+      center: true,
+      padding: '2rem'
+    }
+  },
+  plugins: [],
+}
+
